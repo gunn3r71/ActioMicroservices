a87af7142b6d7e748da1b1bcfea81fdcdd593221
Lucas <lucasgunner3@gmail.com>
2021-12-26 23:57:34 -0300

    Configurando host personalizado

---
+++ src/Actio.Common/RabbitMq/Extensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using System.Threading.Tasks;
+using Actio.Common.Commands;
+using Actio.Common.Events;
+using RawRabbit;
+using RawRabbit.Pipe.Middleware;
+
+namespace Actio.Common.RabbitMq
+{
+    public static class Extensions
+    {
+        public static Task WithCommandHandlerAsync<TCommand>(this IBusClient bus, ICommandHandler<TCommand> handler)
+            where TCommand : ICommand
+            => bus.SubscribeAsync<TCommand>(msg => handler.HandleAsync(msg),
+                                            x => x.UseConsumeConfiguration(config => 
+                                                config.FromQueue(GetQueueName<TCommand>())));
+
+        public static Task WithEventHandlerAsync<TEvent>(this IBusClient bus, IEventHandler<TEvent> handler) 
+            where TEvent : IEvent
+            => bus.SubscribeAsync<TEvent>(msg => handler.HandleAsync(msg),
+                x => x.UseConsumeConfiguration(config =>
+                    config.FromQueue(GetQueueName<TEvent>())));
+
+        private static string GetQueueName<T>()
+            => $"{Assembly.GetEntryAssembly()?.GetName()}/{typeof(T).Name}";
+    }
+}
