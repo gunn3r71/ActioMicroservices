26a1f0baa690d72014154a0b34f9d04a49f2d8f3
Lucas <lucasgunner3@gmail.com>
2021-12-29 18:26:27 -0300

    Implementando repositório de categoria

---
+++ src/Actio.Services.Activities/Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Actio.Services.Activities.Domain.Models;
+using Actio.Services.Activities.Domain.Repositories;
+using MongoDB.Driver;
+
+namespace Actio.Services.Activities.Data.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly IMongoDatabase _db;
+        private IMongoCollection<Category> _collection => _db.GetCollection<Category>("Categories");
+
+        public CategoryRepository(IMongoDatabase db)
+        {
+            _db = db;
+        }
+
+        public async Task<Category> GetAsync(string name) =>
+            await _collection.Find(x => x.Name.ToLowerInvariant() == name.ToLowerInvariant()).FirstOrDefaultAsync();
+        
+        public async Task<IEnumerable<Category>> BrowseAsync() =>
+            await _collection.AsQueryable().ToListAsync();
+
+        public async Task AddAsync(Category category) =>
+            await _collection.InsertOneAsync(category);
+    }
+}
